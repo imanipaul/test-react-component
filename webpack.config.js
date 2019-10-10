807eed8e16ce145c787c6ea14112e58b42dc7b35
Imani Paul <imanipaul@imanis-mbp.lan>
2019-10-09 22:21:51 -0400

    Complete working component

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
